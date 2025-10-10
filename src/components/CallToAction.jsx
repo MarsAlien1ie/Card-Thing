8e5e5aa21898165afef862c96fe818a465ec9530
D. H. <93092744+01DH01@users.noreply.github.com>
2025-10-09 22:19:06 -0400

    Add files via upload

---
+++ src/components/CallToAction.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const CallToAction = () => {
+    return (
+        <section className="cta">
+            <div className="cta-box">
+                <h2>Ready to Become a Pokémon Master?</h2>
+                <div class="gradient-separator-footer"></div>
+                <div className="title-underline-center"></div>
+                <p>
+                    Join thousands of trainers who are already
+                    using Card Cataloger to manage their collections
+                </p>
+                <button className="btn btn-red">Get Started</button>
+            </div>
+        </section>
+    );
+};
+
+export default CallToAction;
---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className="header">
+            <nav className="nav-links">
+                <a href="#features">Features</a>
+                <button class="get-started-button">
+                    Get Started
+                </button>
+            </nav>
+        </header>
+    );
+};
+
+export default Header;

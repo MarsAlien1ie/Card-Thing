7f157e02fd1ec21e9e50c5fdaba92d60e62d01d3
D. H. <93092744+01DH01@users.noreply.github.com>
2025-10-12 15:19:46 -0400

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import React from "react";
 import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import SignPage from "./pages/SignPage";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
-  return <LandingPage />;
+  return (
+    <Routes>
+      <Route path="/" element={<LandingPage />} />
+      <Route path="/login" element={<LoginPage />} />
+      <Route path="/signup" element={<SignPage />} />
+    </Routes>
+  );
 }
 
 export default App;
